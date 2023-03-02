3d53a163e063054a931e8c04fc35906ffc2b64f1
santhosh37 <santhoshcse037@gmail.com>
2023-03-02 14:48:17 +0530

    code clean up changes

--- models/tagModel.js
+++ models/tagModel.js
@@ -1,16 +1,6 @@
 const mongoose = require("mongoose");
 const shortid = require("shortid");
 
-// const formattedDatetime = datetime.toLocaleString("en-IN", {
-//   day: "numeric",
-//   month: "long",
-//   year: "numeric",
-//   hour: "numeric",
-//   minute: "numeric",
-//   second: "numeric",
-//   hour12: true,
-// });
-
 const tagSchema = new mongoose.Schema({
   _id: { type: String, default: shortid.generate },
   name: { type: String, required: true },
